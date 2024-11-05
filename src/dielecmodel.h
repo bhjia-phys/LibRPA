efab3ee364f1b63d8a69159777290ea30034bf34
Huanjing Gong <gonghuanjing@iphy.ac.cn>
2024-11-05 21:10:09 +0800

    Fix bugs: descending order of diagonalization

--- src/dielecmodel.h
+++ src/dielecmodel.h
@@ -7,6 +7,7 @@
 #include "constants.h"
 #include "envs_io.h"
 #include "envs_mpi.h"
+#include "lebedev-quadrature/lebedev_quadrature.hpp"
 #include "libri_stub.h"
 #include "matrix_m_parallel_utils.h"
 #include "meanfield.h"
@@ -30,15 +31,23 @@ struct DoubleHavriliakNegami
 class diele_func
 {
    private:
-    // ( alpha, beta, omega )
-    std::vector<std::vector<std::vector<std::complex<double>>>> head;
-    // ( alpha, mu:n_abfs, omega ) for comparison with FHI-aims
-    std::vector<std::vector<std::vector<std::complex<double>>>> wing_mu;
-    // ( alpha, mu:n_lambda, omega )
-    std::vector<std::vector<std::vector<std::complex<double>>>> wing;
-    // ( lambda: n_abfs-n_singular-1, mu: n_abfs)
+    // ( omega, alpha * beta  )
+    std::vector<matrix_m<std::complex<double>>> head;
+    // ( omega, mu:n_abfs * alpha ) for comparison with FHI-aims
+    std::vector<matrix_m<std::complex<double>>> wing_mu;
+    // ( omega, lambda:n_lambda * alpha)
+    std::vector<matrix_m<std::complex<double>>> wing;
+    // std::vector<std::vector<std::vector<std::complex<double>>>> wing;
+
+    // ( i:n_lambda, j:n_lambda )
+    matrix_m<std::complex<double>> body_inv;
+    // ( i:3, j:3 )
+    matrix_m<std::complex<double>> Lind;
+    // ( i:n_lambda, j:n_lambda )
+    matrix_m<std::complex<double>> chi0;
+    // ( lambda: n_nonsingular-1, mu: n_abfs)
     std::vector<std::vector<std::complex<double>>> Coul_vector;
-    // ( lambda: n_abfs-n_singular-1 )
+    // ( lambda: n_nonsingular-1 )
     std::vector<std::complex<double>> Coul_value;
     // ( mu: n_abfs, m: n_bands, n: n_bands, k )
     std::vector<std::vector<std::vector<std::map<Vector3_Order<double>, std::complex<double>>>>>
@@ -48,15 +57,20 @@ class diele_func
     Cs_LRI_clx Ctri_ij;
 
     MeanField &meanfield_df;
-    const std::vector<double> &omega;
-    const std::vector<Vector3_Order<double>> &kfrac_band;
-    const int n_basis, n_states, n_spin;
+    std::vector<double> omega;
+    std::vector<Vector3_Order<double>> &kfrac_band;
+    int n_basis, n_states, n_spin, n_abf, nk;
     size_t n_nonsingular;
+    // lebedev-quadrature, qw has absorbed 4Pi.
+    std::vector<double> qx_leb, qy_leb, qz_leb, qw_leb;
+    // gamma reciprocal lattice vector, (27-1)*3
+    std::vector<Vector3_Order<double>> g_enclosing_gamma;
+    std::vector<double> q_gamma;
+    double vol_gamma;
 
    public:
-    diele_func(MeanField &mf, const std::vector<Vector3_Order<double>> &kfrac,
-               const std::vector<double> &frequencies_target, const int nbasis, const int nstates,
-               const int nspin)
+    diele_func(MeanField &mf, std::vector<Vector3_Order<double>> &kfrac,
+               std::vector<double> frequencies_target, int nbasis, int nstates, int nspin)
         : meanfield_df(mf),
           kfrac_band(kfrac),
           omega(frequencies_target),
@@ -64,11 +78,14 @@ class diele_func
           n_states(nstates),
           n_spin(nspin)
     {
-        init_headwing();
+        init();
     };
+    diele_func() : meanfield_df(meanfield), kfrac_band(kfrac_list) {};
     ~diele_func() {};
-    void init_headwing();
+    void init();
     void init_Cs();
+    void set(MeanField &mf, std::vector<Vector3_Order<double>> &kfrac,
+             std::vector<double> frequencies_target, int nbasis, int nstates, int nspin);
 
     void cal_head();
     double cal_factor(string name);
@@ -87,4 +104,17 @@ class diele_func
     // diagonalize Vq_cut(q=0)
     void get_Xv();
     void test_wing();
+
+    void get_body_inv(matrix_m<std::complex<double>> &chi0_block);
+    void construct_L(const int ifreq);
+    // Lebedev-Laikov quadrature
+    void get_Leb_points();
+    void get_g_enclosing_gamma();
+    void calculate_q_gamma();
+    void cal_eps(const int ifreq);
+    std::complex<double> compute_chi0_inv_00(const int ifreq);
+    std::complex<double> compute_chi0_inv_ij(const int ifreq, int i, int j);
+    void rewrite_eps(matrix_m<std::complex<double>> &chi0_block, const int ifreq);
 };
+
+extern diele_func df_headwing;
